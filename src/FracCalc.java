6d58d6f12a70305f789d822178cb281e054ca5ab
s-paschoj <s-paschoj@49S50020832.it.bsd405.org>
2018-12-13 22:33:59 -0800

    Final Checkpoint, multi-operation tests don't work unfortunately

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -1,6 +1,5 @@
 import java.util.*;
 public class FracCalc {
-
     /**
      * Prompts user for input, passes that input to produceAnswer, then outputs the result.
      * @param args - unused
@@ -12,7 +11,7 @@ public class FracCalc {
         // Checkpoint 2: Accept user input multiple times.
     	Scanner console = new Scanner(System.in);
     	String input = " ";
-    	while (!(input.equals("0 + 0"))) {
+    	while (!(input.equals("quit"))) {
     		System.out.println("Enter equation:");
     		input = console.nextLine();
     		String y =produceAnswer(input);
@@ -49,7 +48,6 @@ public class FracCalc {
     	// the operand is one space after the first number
     	String operator = input.substring(x+1,x+2);
     	// the second number is one space after the operand, I don't know how long this number is so I leave the substring end 'open' to make sure I don't cut it off
-    	
     	String second_num = input.substring(x+3);
     	String whole_num_1 = find_whole_number(first_num);
     	String numerator_1 = find_numerator(first_num);
@@ -57,6 +55,7 @@ public class FracCalc {
     	String whole_num_2 = find_whole_number(second_num);
     	String numerator_2 = find_numerator(second_num);
     	String denominator_2 = find_denominator(second_num);  
+    	//Converting the strings to Integers makes simplifiable and reducable 
     	int wn1 = Integer.parseInt(whole_num_1);
     	int n1 = Integer.parseInt(numerator_1);
     	int d1 = Integer.parseInt(denominator_1);
@@ -73,7 +72,7 @@ public class FracCalc {
     	// If num is a mixed number, convert to normal fraction
     		n1 = MixedToFrac(wn1,n1,d1);
 			n2 = MixedToFrac(wn2,n2,d2);
-    		
+    		wn1 = 0;
         	// if denominators are not the same, covert them.
         	if (d1 != d2) {
         		int temp_1 = d1;
@@ -85,15 +84,15 @@ public class FracCalc {
         	} 
         	n1+=n2;
         	//Combine Numerator and Denominator for return
-        	String answer = (Integer.toString(n1) + "/" + Integer.toString(d1));
         	//If both the numerator and Denominator are 0, return 0 instead of 0 + 0 
         	if (n1 == 0 || d1 == 0) {
         		return "0";
         	//Otherwise, return the answer as normal
         	} else
-        		return answer;
+        		return simplify(wn1,n1,d1);
 //------------------------------------------------------------------------------------------------------------------------------------------------
     	} else if (operator.equals("-")) {
+    		// If both denominators are 1, then just treat the inputs as whole numbers
     		if (d1 == 1 && d2 == 1) {
     			int sum = wn1-wn2;
     			String ret = Integer.toString(sum);
@@ -102,8 +101,9 @@ public class FracCalc {
     	// If num1 is a mixed number, convert to normal fraction
     		n1 = MixedToFrac(wn1,n1,d1);
 			n2 = MixedToFrac(wn2,n2,d2);  	
-    		
+    		wn1 = 0;
         	// if denominators are not the same, covert them.
+    		// Temp variables are used so that I don't convert a number before I need to use it for other numbers
         	if (d1 != d2) {
         		int temp_1 = d1;
         		int temp_2 = d2;
@@ -113,6 +113,17 @@ public class FracCalc {
         		n2*=temp_1;	
         	} 
         	n1-=n2;
+        	//If the numerator is greater than the denominator, convert to a mixed fraction
+    		if (Math.abs(n1)>Math.abs(d1)) {
+    			return simplify(wn1,n1,d1);
+    		}
+    		//Method for reducing, n3 and d3 are used so that I don't change a variable I need for reduction
+    		if (n1 != 0 && n2 != 0) {
+    			int n3 = reduce(n1,d1);
+    			int d3 = reduce(d1,n1);
+            	String answer = (Integer.toString(n3) + "/" + Integer.toString(d3));
+            	return answer;
+    		}
         	String answer = (Integer.toString(n1) + "/" + Integer.toString(d1));
         	//If both the numerator and denominator are 0, return 0 instead of 0 - 0
         	if (n1 == 0 || d1 == 0) {
@@ -122,17 +133,28 @@ public class FracCalc {
     	
 //------------------------------------------------------------------------------------------------------------------------------------------------        	
     	} else if (operator.equals("*")) {
+    		//If both numerators are 0, multiply the whole numbers
     		if (n1 == 0 && n2 == 0) {
     			wn1 *= wn2;
     			return (Integer.toString(wn1));
     					
     		}
+    		
         	// If num1 is a mixed number, convert to normal fraction 
     		n1 =MixedToFrac(wn1,n1,d1);
     		n2 =MixedToFrac(wn2,n2,d2);
-        	
+        	wn1 = 0;
     		n1*=n2;
     		d1*=d2;
+    		//If numerator is greater than denominator, convert to mixed number
+    		if (Math.abs(n1)>Math.abs(d1)) {
+    			return simplify(wn1,n1,d1);
+    		}
+    		//Reduce 
+    		if (n1 != 0 && n2 != 0) {
+    			n1 = reduce(n1,d1);
+    			d1 = reduce(d1,n1);
+    		}
         	String answer = (Integer.toString(n1) + "/" + Integer.toString(d1));
         	//If both the numerator and denominator are 0, return 0 instead of 0 * 0
         	if (n1 == 0 || d1 == 0) {
@@ -141,20 +163,65 @@ public class FracCalc {
         		return answer;
 //-------------------------------------------------------------------------------------------------------------------------------------------------
     	} else if (operator.equals("/")) {
-    		if (n1 == 0 && n2 == 0 && wn1 > 0 && wn2 > 0) {
+    		if (wn1 !=0 && wn2 != 0) {
+    			if (wn1 == wn2) {
+    				return "1";
+    			}
+    		}
+    		//Any number divided by 1 will be itself
+    		if (wn2 == 1) {
+    			return (Integer.toString(wn1));
+    		} else if (wn2 == -1) {
+    			wn1 *= -1;
+    			return (Integer.toString(wn1));
+    		}
+    		//If both numbers are negative the answer will be positive
+    		if (wn1 < 0 && wn2 < 0) {
+    			wn1*=-1;
+    			wn2*=-1;
+    		}
+    		//If the whole number and one of the other numbers is 0, the answer will be 0
+    		if (Math.abs(wn1) == 0) {
+	    		if (Math.abs(n1) == 0 || Math.abs(d1) == 0) {
+	    			return "0";
+	    		}
+    		}
+    		//Process for whole numbers
+    		if (n1 == 0 && n2 == 0 && wn1 != 0 && wn2 != 0) {
+    			n1 = wn1;
+    			d1 = wn2;		
+        		if (Math.abs(n1)>Math.abs(d1)) {
+        			return simplify(wn1,n1,d1);
+        		}
            		String answer = (Integer.toString(wn1)+ "/" + Integer.toString(wn2));
     			return answer;
+    			// if input is 2 negative whole numbers
     		} else if (n1 == 0 && n2 == 0 && wn1 < 0 && wn2 < 0) {
     			wn2 *= -1;
+    			wn1 *= -1;
+        		if (Math.abs(n1)>Math.abs(d1)) {
+        			return simplify(wn1,n1,d1);
+        		}
     			String answer = (Integer.toString(wn1)+ "/" + Integer.toString(wn2));
     			return answer;
     		}
     		//If the fraction is a mixed number, convert it to a normal fraction
     		n1 =MixedToFrac(wn1,n1,d1);
     		n2 =MixedToFrac(wn2,n2,d2);  
-        	
+        	wn1 = 0;
+        	if (n2 <0) {
+        		n1*=d2;
+        		n1*=-1;
+        		d1*=n2;
+        		d1*=-1;
+        	} else {
     		n1*=d2;
     		d1*=n2;
+        	}
+        	//Simplify
+    		if (n1 != 0 && d1 !=0) {
+    			return simplify(wn1,n1,d1);
+    		}
 			String answer = (Integer.toString(n1)+ "/" + Integer.toString(d1));
 			//if the answer would be 0 / 0, return 0
     		if (n1 == 0 && d1 == 0) {
@@ -247,9 +314,51 @@ public class FracCalc {
 		}
 		return n1;
 		}
+        //Method for Simplifying fractions
+        public static String simplify(int wn, int n, int d) {
+        	if (Math.abs(n) >= Math.abs(d)) {
+        		// whole number is equal to n/d with no remainder
+        		wn = n/d;
+        		// Numerator is equal to remainder
+        		n = n % d;
+        	}
+        	if (d<0) {
+        		//Denominator should never be negative
+        		d *= -1;
+        	}
+        	if (n<0 && wn >0) {
+        		//If Numerator is negative but whole number isn't, switch it so it's correct
+        		n*= -1;
+        		wn*= -1;
+        	}
+        		//If both numerator and whole number are negative, change numerator to positive so it's correct
+        	if (n<0 && wn<0) {
+        		n*= -1;
+        	}
+        	//reduce, ns and ds stand for numerator/denominator simplified 
+        	int ns = reduce(n,d);
+        	int ds = reduce(d,n);
+        	if (ns == ds) {
+        		return "1";
+        	}
+        	//If the whole number is 0, don't have the 0_ in the return
+        	if (wn == 0) {
+        		return (Integer.toString(ns) + "/" + Integer.toString(ds));
+        		//If both numerator and denominator are 0, return the whole number
+        	} else if (ns == 0 || ds == 0) {
+        		return Integer.toString(wn);
+        	}
+        	//Final return
+        	return (Integer.toString(wn) + "_" + Integer.toString(ns) + "/" + Integer.toString(ds));
+        }
     
-    
-    
+        public static int reduce(int x, int y) {
+        	int gcd = greatestCommonDivisor(x,y);
+        	if (gcd != 0) {
+        		return x/gcd;
+        	} else
+        		return x;
+        	}
     /**
      * greatestCommonDivisor - Find the largest integer that evenly divides two integers.
      *      Use this helper method in the Final Checkpoint to reduce fractions.
@@ -271,7 +380,6 @@ public class FracCalc {
         }
         return max;
     }
-    
     /**
      * leastCommonMultiple - Find the smallest integer that can be evenly divided by two integers.
      *      Use this helper method in Checkpoint 3 to evaluate expressions.
